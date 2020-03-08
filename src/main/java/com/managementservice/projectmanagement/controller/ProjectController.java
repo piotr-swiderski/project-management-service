d98e63eb91ae14f6f7dcdd212e8d08635fa5f47d
kruk1 <kruk162@gmail.com>
2020-03-08 21:42:30 +0100

    list project skończyć

--- src/main/java/com/managementservice/projectmanagement/controller/ProjectController.java
+++ src/main/java/com/managementservice/projectmanagement/controller/ProjectController.java
@@ -3,13 +3,16 @@ package com.managementservice.projectmanagement.controller;
 import com.managementservice.projectmanagement.entity.Project;
 import com.managementservice.projectmanagement.entity.User;
 import com.managementservice.projectmanagement.repositorie.ProjectRepository;
+import com.managementservice.projectmanagement.repositorie.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -17,8 +20,11 @@ public class ProjectController {
 
     @Autowired
     ProjectRepository projectRepository;
+    @Autowired
+    UserRepository userRepository;
 
     String NEW_PROJECT_PAGES = "newProject";
+    String LIST_PROJECT_PAGES = "myProjectList";
 
     @GetMapping("/newProject")
     public String newProject() {
@@ -26,11 +32,27 @@ public class ProjectController {
     }
 
     @PostMapping("/newProject")
-    public String newProjectForm(@RequestParam String name, @RequestParam String description, @AuthenticationPrincipal User user) {
-        Project project = new Project(name, description, user);
+    public String newProjectForm(@RequestParam String name, @RequestParam String description) {
+        Project project = new Project(name, description, getUserAuthentication());
         projectRepository.save(project);
 
-        return "index";
+     return "index";
     }
+
+    @GetMapping ("/myProject")
+    public String myProjectList (@RequestParam String username, Model model) {
+        model.addAttribute("projects", projectRepository.findAllByUsers(getUserAuthentication().getUsername()) );
+        return LIST_PROJECT_PAGES;
+    }
+
+
+    public User getUserAuthentication () {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String userName = auth.getName();
+        return  userRepository.findByUsername(userName);
+    }
+
+
+
 
 }
--- src/main/java/com/managementservice/projectmanagement/repositorie/ProjectRepository.java
+++ src/main/java/com/managementservice/projectmanagement/repositorie/ProjectRepository.java
@@ -3,7 +3,12 @@ package com.managementservice.projectmanagement.repositorie;
 import com.managementservice.projectmanagement.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    Set<Project> findAllByUsers (String Username);
+
 
 
 }

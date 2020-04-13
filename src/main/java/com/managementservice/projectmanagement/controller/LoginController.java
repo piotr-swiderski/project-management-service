cdaed4cbee6870d78ffe84ef957e30e4e1343137
kruk1 <kruk162@gmail.com>
2020-04-13 11:52:20 +0200

    adding message failure error login page

--- src/main/java/com/managementservice/projectmanagement/controller/LoginController.java
+++ src/main/java/com/managementservice/projectmanagement/controller/LoginController.java
@@ -4,9 +4,12 @@ import com.managementservice.projectmanagement.service.UserService;
 import com.managementservice.projectmanagement.utils.AccountTypeEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import static com.managementservice.projectmanagement.utils.ControllerUtil.*;
 
 @Controller
 public class LoginController {
@@ -24,6 +27,19 @@ public class LoginController {
     public String getLoginPage() {
         return LOGIN_PAGE;
     }
+
+    @GetMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute(ERROR_LOGIN_BAD_CREDENSIALS, ERROR_LOGIN_BAD_CREDENSIALS_MESSAGE);
+        return LOGIN_PAGE;
+    }
+
+    @GetMapping("/login-error-oauth2")
+    public String loginErrorOauth2(Model model) {
+        model.addAttribute(ERROR_LOGIN_BAD_CREDENSIALS, ERROR_LOGIN_OAUTH2_BAD_CREDENSIALS_MESSAGE);
+        return LOGIN_PAGE;
+    }
+
 
     @PostMapping("/signup")
     public String singUp(@RequestParam(value = "username") String username,
--- src/main/java/com/managementservice/projectmanagement/utils/ControllerUtil.java
+++ src/main/java/com/managementservice/projectmanagement/utils/ControllerUtil.java
@@ -2,8 +2,16 @@ package com.managementservice.projectmanagement.utils;
 
 public class ControllerUtil {
 
+    //login error
+    public static final String ERROR_LOGIN_BAD_CREDENSIALS = "error";
+    public static final String ERROR_LOGIN_BAD_CREDENSIALS_MESSAGE = "wrong password or login";
+    public static final String ERROR_LOGIN_OAUTH2_BAD_CREDENSIALS_MESSAGE = "Authorization error please try again later";
+
+    // sprint error
     public static final String ERROR_ADDING_SPRINT_HANDLER = "error";
     public static final String ERROR_ADDING_SPRINT_MESSAGE = "Your Sprint date is not valid";
+
+    //notification error
     public static final String ERROR_ADDING_NOTIFICATION = "error";
     public static final String ERROR_ADDING_NOTIFICATION_MESSAGE = "Failed to add user";
     public static final String ERROR_ADDING_NOTIFICATION_USERS = "error";
@@ -12,16 +20,17 @@ public class ControllerUtil {
     public static final String ERROR_DELETE_USERS_TO_PROJECT_MESSAGE = "Unable to delete project admin";
 
 
+
     public static final String PROJECT_HANDLER = "project";
     public static final String TASK_LIST = "tasksList";
     public static final String SPRINT_HANDLER = "sprint";
 
 
-    public static final String SUCCSES_ADDING_NOTIFICATION = "succes";
-    public static final String SUCCSES_ADDING_NOTIFICATION_MESSAGE = "Notification sent";
-
+    // notification Succes
     public static final String SUCCSES_DELETE_USER = "succes";
     public static final String SUCCSES_DELETE_USER_MESSAGE = "User removed from project";
+    public static final String SUCCSES_ADDING_NOTIFICATION = "succes";
+    public static final String SUCCSES_ADDING_NOTIFICATION_MESSAGE = "Notification sent";
 
 
     public static final String ERROR_HANDLER = "ERROR_HANDLER";

aee4215d301333a281d608cb798d7967bd4a9bcb
Piotr Świderski <team1996@interia.pl>
2020-04-22 22:19:43 +0200

    LoginController test

---
+++ src/test/java/com/managementservice/projectmanagement/controller/SprintRestControllerTest.java
@@ -0,0 +1,32 @@
+package com.managementservice.projectmanagement.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SprintRestControllerTest {
+
+    @Test
+    void changeTaskTable() {
+    }
+
+    @Test
+    void createTaskErrand() {
+    }
+
+    @Test
+    void getTaskErrands() {
+    }
+
+    @Test
+    void setErrandChecked() {
+    }
+
+    @Test
+    void setTaskDescription() {
+    }
+
+    @Test
+    void setTaskName() {
+    }
+}
